7cf94470551fd3c0ef10b11211e388b46b725977
Andrew <andrew@andrewbritton.com>
2016-12-28 14:04:21 -0800

    Added "print" and "print_corners" functions to template<char/str>
    The Matrix<char / str> classes have the print() and print_corners() functions to give a reminder to wayward users of the class that the object was improperly instantiated.

--- Simple_Matrix/Source.cpp
+++ Simple_Matrix/Source.cpp
@@ -1,3 +1,4 @@
+#include <iomanip>
 #include <vector>
 #include <cmath>
 #include <iostream>
@@ -17,7 +18,11 @@ public:
 	Matrix(std::vector<T> &rhs) : data(rhs)
 	{
 		data = rhs;
-		this->size_dim();
+		if (!this->size_dim())
+		{
+			this->clear();
+			std::cout << "::ERROR:: Matrix<T> cannot initialize with non-square matrix\n";
+		}
 	}
 	Matrix(T in)
 	{
@@ -40,7 +45,7 @@ public:
 		long long counter = 1;
 		for (auto &&e : this->data)
 		{
-			std::cout << e << ", ";
+			std::cout <<std::setprecision(6) << e << ", ";
 			if (counter % dim == 0)
 				std::cout << "\n";
 			counter++;
@@ -53,9 +58,9 @@ public:
 		std::cout << "-----\nMatrix Data: Corners\n-----\n";
 		std::cout << "-- Matrix Dim: " << this->dim << "\n-----\n";
 
-		std::cout << this->data[0] << " -- " << this->data[dim - 1] << "\n";
+		std::cout << std::setprecision(6) << this->data[0] << " -- " << this->data[dim - 1] << "\n";
 		std::cout << "|    |\n";
-		std::cout << this->data[dim * dim - dim] << " -- " << this->data[dim*dim - 1] << "\n";
+		std::cout << std::setprecision(6) << this->data[dim * dim - dim] << " -- " << this->data[dim*dim - 1] << "\n\n";
 	}
 
 	void transpose()
@@ -91,9 +96,15 @@ public:
 	}
 
 private:
-	inline void size_dim()
+	inline bool size_dim()
 	{
+		bool OUT = false;
 		this->dim = unsigned int(std::sqrt(this->data.size()));
+		if (dim * dim == data.size())
+			OUT = true;
+		
+		// no else statement since OUT is initialized as false
+		return OUT;
 	}
 
 	std::vector<T> data;
@@ -110,7 +121,23 @@ public:
 		std::cout << "::ERROR:: Matrix can't initialize with type <CHAR>.\n";
 	}
 
+	void print()
+	{
+		std::cout << "-----\nMatrix Data\n-----\n";
+		std::cout << "-- Matrix Dim: " << 0 << "\n-----\n";
+		std::cout << "-----\n\n";
+	}
+
+	void print_corners()
+	{
+		std::cout << "-----\nMatrix Data: Corners\n-----\n";
+		std::cout << "-- Matrix Dim: " << 0 << "\n-----\n";
+		std::cout << "-----\n\n";
+	}
+
 private:
+	// data and dim attributes are not included, to ensure that an improperly created class
+	// will not accidentally be put to use in production code later
 };
 
 
@@ -123,7 +150,23 @@ public:
 		std::cout << "::ERROR:: Matrix can't initialize with type <STD::STRING>.\n";
 	}
 
+	void print()
+	{
+		std::cout << "-----\nMatrix Data\n-----\n";
+		std::cout << "-- Matrix Dim: " << 0 << "\n-----\n";
+		std::cout << "-----\n\n";
+	}
+
+	void print_corners()
+	{
+		std::cout << "-----\nMatrix Data: Corners\n-----\n";
+		std::cout << "-- Matrix Dim: " << 0 << "\n-----\n";
+		std::cout << "-----\n\n";
+	}
+
 private:
+	// data and dim attributes are not included, to ensure that an improperly created class
+	// will not accidentally be put to use in production code later
 };
 
 
@@ -133,16 +176,32 @@ int main()
 	//Check floats
 	std::cout << "\n............\n Check FLOATs\n";
 	std::vector<float> mtx_array = { 1.0f, 2.0f, 3.3f, 4.4f, 5.5f, \
-		6.6f, 7.7f, 8.8f, 9.9f, 10.1f, \
-		11.4f, 12.2f, 13.3f, 14.4f, 15.5f, \
-		16.6f };
-
+								   	6.6f, 7.7f, 8.8f, 9.9f, 10.1f, \
+								   	11.4f, 12.2f, 13.3f, 14.4f, 15.5f, \
+								   	16.6f };
 	Matrix<float> mtx(mtx_array);
 	mtx.print();
 	mtx.print_corners();
 	mtx.transpose();
 	mtx.print();
 	mtx.print_corners();
+
+
+	//Check single double
+	std::cout << "\n............\n Check Single DOUBLE\n";
+	Matrix<double> mtx_single_double(34.0004);
+	mtx_single_double.print();
+	mtx_single_double.transpose();
+	mtx_single_double.print();
+
+
+	//Check Non Square list
+	std::cout << "\n............\n Check NON Square MTX\n";
+	std::vector<double> mtx_double_array = {97.346335, 872.982734, 98.2398235, -.0293982, -29083.2352};
+	Matrix<double> mtx_double(mtx_double_array);
+	mtx_double.print();
+	mtx_double.transpose();
+	mtx_double.print();
 
 
 	//Check Single item
@@ -158,18 +217,20 @@ int main()
 	Matrix<float> mtx_float_2 = mtx;
 	mtx_float_2.print();
 
+
 	//Check Ints
 	std::cout << "\n............\n Check INTs\n";
 	std::vector<int> int_mtx_array = { 1, 2, 3, 4, 5, 6, \
-		7, 8, 9, 10, 11, 12, \
-		13, 14, 15, 16, 17, 18, \
-		19, 20, 21, 22, 23, 24, \
-		25, 26, 27, 28, 29, 30, \
-		31, 32, 33, 34, 35, 36 };
+										7, 8, 9, 10, 11, 12, \
+										13, 14, 15, 16, 17, 18, \
+										19, 20, 21, 22, 23, 24, \
+										25, 26, 27, 28, 29, 30, \
+										31, 32, 33, 34, 35, 36 };
 	Matrix<int> mtx_int(int_mtx_array);
 	mtx_int.print();
 	mtx_int.transpose();
 	mtx_int.print();
+
 
 	//Check Large Squares of LONG LONG
 	std::cout << "\n............\n Check LONG LONGs\n";
